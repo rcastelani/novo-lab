94a40c4449d643036f80f7a1f9b180abc4ec3caa
rcastelani <castelani.rodrigo@gmail.com>
2021-03-24 18:21:20 -0300

    new-lab

---
+++ main.tf
@@ -0,0 +1,71 @@
+locals {
+//  vpc_id           = "vpc-0548d408bf3549ca0"
+//  subnet_id        = "subnet-060a1ae52cf0a73d6"
+  ssh_user         = "ubuntu"
+  key_name         = "lab-tf-key"
+  private_key_path = "~/lab-tf-key.pem"
+}
+
+provider "aws" {
+  region = var.aws_lab_region
+}
+
+## Create VPC ##
+resource "aws_vpc" "vpc-lab" {
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_hostnames = true
+  name                 = "vpc-lab"
+}
+
+## Create Subnets ##
+resource "aws_subnet" "subnet_1-lab" {
+  vpc_id            = aws_vpc.vpc-lab.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "sa-east-1a"
+  name              = "subnet1-lab"
+}
+
+resource "aws_security_group" "nginx" {
+  description = "Allow limited inbound external traffic"
+  name   = "nginx_access"
+  vpc_id = aws_vpc.vpc-lab.id
+
+  dynamic "ingress" {
+  for_each = var.default_ingress
+    content {
+      description = ingress.value["description"]
+      from_port   = ingress.key
+      to_port     = ingress.key
+      protocol    = "tcp"
+      cidr_blocks = ingress.value["cidr_blocks"]
+    }
+  }
+}
+
+resource "aws_instance" "nginx" {
+  ami                         = var.aws_lab_ami
+  subnet_id                   = aws_subnet.subnet_1-lab.id
+  instance_type               = var.aws_lab_instance_type
+  associate_public_ip_address = true
+  security_groups             = [aws_security_group.nginx.id]
+  key_name                    = local.key_name
+
+  provisioner "remote-exec" {
+    inline = ["echo 'Wait until SSH is ready'"]
+
+    connection {
+      type        = "ssh"
+      user        = local.ssh_user
+      private_key = file(local.private_key_path)
+      host        = aws_instance.nginx.public_ip
+    }
+  }
+  provisioner "local-exec" {
+    command = "ansible-playbook  -i ${aws_instance.nginx.public_ip}, --private-key ${local.private_key_path} nginx.yaml"
+  }
+}
+
+output "nginx_ip" {
+  value = aws_instance.nginx.public_ip
+}
---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable default_ingress {
+  type = map(object({description = string, cidr_blocks = list(string)}))
+  default = {
+    22 = { description = "Inbound para SSH", cidr_blocks = [ "127.0.0.1/32" ]}
+    80 = { description = "Inbound para HTTP", cidr_blocks = [ "127.0.0.1/32" ]}
+  } 
+}
+
+variable "aws_lab_ami" {
+  description = "def image ami in main.tf"
+  default = "ami-01eb71e14cef400e2"
+}
+
+variable "aws_lab_instance_type" {
+  description = "def instance type name in main.tf"
+  default = "t2.micro"
+}
+
+variable "aws_lab_region" {
+  description = "def region in main.tf"
+  default= "sa-east-1"
+}
